5c0533480bc57c89208637ec663fdaf9f9111376
Raul Almeida <haltsimog@gmail.com>
2020-12-07 22:37:53 -0300

    q5 might not be done

--- queries/q5_qtd_encomendas.sql
+++ queries/q5_qtd_encomendas.sql
@@ -1,1 +1,1 @@
-select n_name, count(*) from orders inner join (nation inner join customer on n_nationkey = c_nationkey) on c_custkey group by n_nationkey;
+select n_name, count(*) from orders inner join (nation inner join customer on n_nationkey = c_nationkey) on c_custkey group by n_nationkey order by count(*) desc;
