e55962f881ec223e416a3be6183eb0834f5805bb
Raul Almeida <haltsimog@gmail.com>
2020-12-16 20:38:30 -0300

    add q8

--- queries/q8_not_special.sql
+++ queries/q8_not_special.sql
@@ -1,18 +1,7 @@
---clientes que n tem order + clientes que tem order, mas n tem comment specialrequest
-
---pegando os que não tem order:
---SELECT c_custkey FROM CUSTOMER C LEFT JOIN ORDERS O ON C.c_custkey = O.o_custkey WHERE O.o_custkey IS NULL;
-
---pegando os que tem order sem comment special request
---SELECT c_custkey FROM CUSTOMER C INNER JOIN ORDERS O ON C.c_custkey = O.o_custkey WHERE O.o_comment LIKE "%special request%"
-
---faz união das duas anteriores, conta quantos custkey conseguiu
-
-
-
---RESPOSTA ABAIXO?
-SELECT COUNT(DISTINCT c_custkey) FROM (
-	SELECT c_custkey FROM CUSTOMER C LEFT JOIN ORDERS O ON C.c_custkey = O.o_custkey WHERE O.o_custkey IS NULL
-	UNION
-	SELECT c_custkey FROM CUSTOMER C INNER JOIN ORDERS O ON C.c_custkey = O.o_custkey WHERE O.o_comment LIKE "%special request%"
+SELECT COUNT(*) FROM (
+	SELECT DISTINCT c_custkey FROM CUSTOMER C 
+	EXCEPT
+	SELECT DISTINCT c_custkey FROM CUSTOMER C INNER JOIN ORDERS O 
+	ON C.c_custkey = O.o_custkey 
+	WHERE O.o_comment LIKE "%special request%"
 );
